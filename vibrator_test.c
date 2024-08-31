cf43d87bb0dd87b3092aaaf901381bceb6340a9f
fangpeina <fangpeina@xiaomi.com>
2024-08-31 21:14:21 +0800

    frameworks/vibrator: add play interval port for quickapp
    Signed-off-by: fangpeina <fangpeina@xiaomi.com>

--- vibrator_test.c
+++ vibrator_test.c
@@ -37,6 +37,8 @@
 #define VIBRATOR_TEST_DEFAULT_API 1
 #define VIBRATOR_TEST_DEFAULT_STRENGTH 2
 #define VIBRATOR_TEST_WAVEFORM_MAX 7
+#define VIBRATOR_TEST_DEFAULT_INTERVAL 1000
+#define VIBRATOR_TEST_DEFAULT_COUNT 5
 
 /****************************************************************************
  * Private Types
@@ -57,6 +59,8 @@ struct vibrator_test_s {
     int es;
     int repeat;
     int waveformid;
+    int interval;
+    int count;
     struct waveform_arrays_s waveform_args[VIBRATOR_TEST_WAVEFORM_MAX];
 };
 
@@ -71,6 +75,7 @@ enum vibrator_test_apino_e {
     VIBRATOR_TEST_GETCAPABILITIES,
     VIBRATOR_TEST_SETINTENSITY,
     VIBRATOR_TEST_GETINTENSITY,
+    VIBRATOR_TEST_INTERVAL,
 };
 
 /****************************************************************************
@@ -91,7 +96,9 @@ static void usage(void)
            "\t            -1 means no repeat, default: -1\n"
            "\t[-i <val> ] The intensity of vibration[0,3], default: 2\n"
            "\t[-s <val> ] The effect strength, [0, 2], default: 2\n"
-           "\t[-l <val> ] The waveform array id, [0, 6], default: 0\n");
+           "\t[-l <val> ] The waveform array id, [0, 6], default: 0\n"
+           "\t[-d <val> ] The interval of vibration in milliseconds, default: 1000\n"
+           "\t[-c <val> ] The count of vibration, default: 5\n");
 }
 
 static int test_play_predefined(uint8_t id, vibrator_effect_strength_e es)
@@ -109,6 +116,11 @@ static int test_paly_waveform(int repeat, struct waveform_arrays_s waveform_args
     printf("repeat = %d, length = %d\n", repeat, waveform_args.length);
     return vibrator_play_waveform(waveform_args.timings,
         waveform_args.amplitudes, repeat, waveform_args.length);
+}
+
+static int test_play_interval(int duration, int interval, int count)
+{
+    return vibrator_play_interval(duration, interval, count);
 }
 
 static int test_play_primitive(uint8_t id, uint16_t amplitude)
@@ -154,7 +166,7 @@ static int param_parse(int argc, char* argv[],
     const char* apino;
     int ch;
 
-    while ((ch = getopt(argc, argv, "t:a:e:r:i:s:l:h")) != EOF) {
+    while ((ch = getopt(argc, argv, "t:a:e:r:i:s:l:d:c:h")) != EOF) {
         switch (ch) {
         case 't': {
             printf("%s\n", optarg);
@@ -205,6 +217,22 @@ static int param_parse(int argc, char* argv[],
                 || test_data->waveformid >= VIBRATOR_TEST_WAVEFORM_MAX) {
                 printf("NOTE: Invalid waveform id, use 0 to 6\n");
                 return -1;
+            }
+            break;
+        }
+        case 'd': {
+            test_data->interval = atoi(optarg);
+            printf("test_data->interval = %d\n", test_data->interval);
+            if (test_data->interval < 0) {
+                printf("NOTE: Invalid interval, use non-negative value\n");
+            }
+            break;
+        }
+        case 'c': {
+            test_data->count = atoi(optarg);
+            printf("test_data->count = %d\n", test_data->count);
+            if (test_data->count < 0) {
+                printf("NOTE: Invalid count, use non-negative value\n");
             }
             break;
         }
@@ -309,6 +337,14 @@ static int do_vibrator_test(struct vibrator_test_s* test_data)
             return ret;
         }
         break;
+    case VIBRATOR_TEST_INTERVAL:
+        printf("API TEST: vibrator_play_interval\n");
+        ret = test_play_interval(test_data->time, test_data->interval, test_data->count);
+        if (ret < 0) {
+            printf("play_interval failed: %d\n", ret);
+            return ret;
+        }
+        break;
     default:
         printf("arg out of range\n");
         break;
@@ -375,6 +411,8 @@ int main(int argc, char* argv[])
     test_data.es = VIBRATOR_TEST_DEFAULT_STRENGTH;
     test_data.time = VIBRATOR_TEST_DEFAULT_TIME;
     test_data.api = VIBRATOR_TEST_DEFAULT_API;
+    test_data.interval = VIBRATOR_TEST_DEFAULT_TIME;
+    test_data.count = VIBRATOR_TEST_DEFAULT_COUNT;
 
     /*Init waveform test arrays*/
     waveform_args_init(&test_data);
